b2854dc81f551bc9a69e77501425fdcfb94215fb
Kartik Bhadouriya <ks8783375@gmail.com>
2025-06-08 02:59:19 +0530

    Update Header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="bg-red-300 text-white shadow-md">
             <div className="container mx-auto px-1 py-1 flex justify-between items-center">
                 {/* Logo */}
-                <a href="index.html" className="flex items-center">
+                <a href="#" className="flex items-center">
                     <img src="/images/logo/logo2.png" alt="Logo" className="h-14" />
                 </a>
 
@@ -47,17 +47,17 @@ const Header = () => {
                         } md:flex md:items-center md:space-x-8 absolute md:static top-full left-0 w-full md:w-auto bg-gray-900 md:bg-transparent z-10 px-4 md:px-0 py-4 md:py-0`}
                 >
                     <ul className="flex flex-col md:flex-row gap-4 md:gap-8 text-white text-base font-medium">
-                        <li><a href="index.html" className="hover:text-red-300">Home</a></li>
-                        <li><a href="shop.html" className="hover:text-gray-300">Shop</a></li>
-                        <li><a href="about.html" className="hover:text-gray-300">About us</a></li>
-                        <li><a href="services.html" className="hover:text-gray-300">Services</a></li>
-                        <li><a href="blog.html" className="hover:text-gray-300">Blog</a></li>
-                        <li><a href="contact.html" className="hover:text-gray-300">Contact us</a></li>
+                        <li><a href="#" className="hover:text-red-300">Home</a></li>
+                        <li><a href="#" className="hover:text-gray-300">Shop</a></li>
+                        <li><a href="#" className="hover:text-gray-300">About us</a></li>
+                        <li><a href="#" className="hover:text-gray-300">Services</a></li>
+                        <li><a href="#" className="hover:text-gray-300">Blog</a></li>
+                        <li><a href="#" className="hover:text-gray-300">Contact us</a></li>
 
                         <a href="#">
                             <img src="/images/user.svg" alt="User" className="h-6" />
                         </a>
-                        <a href="cart.html">
+                        <a href="#">
                             <img src="/images/cart.svg" alt="Cart" className="h-6" />
                         </a>
 
